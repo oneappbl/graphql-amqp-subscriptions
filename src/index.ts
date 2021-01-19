188edef6fa7f8c50551c1cd4b355ffabe78d9d6f
Kiran Patel <kir2kpa@gmail.com>
2021-01-19 16:54:10 -0500

    Revert "Added modules required to extend PubSubAsyncIterator."
    This reverts commit 0b20ec27eecc2ab1bff7e6ecf1be4f2865700fa9.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 /* istanbul ignore file */
 export { AMQPPubSub } from './pubsub';
-export { PubSubAsyncIterator } from './pubsub-async-iterator';
-export { PubSubEngineWithCleanup } from './amqp/interfaces';
