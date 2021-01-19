0b20ec27eecc2ab1bff7e6ecf1be4f2865700fa9
Kiran Patel <kir2kpa@gmail.com>
2021-01-19 16:21:55 -0500

    Added modules required to extend PubSubAsyncIterator.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 /* istanbul ignore file */
 export { AMQPPubSub } from './pubsub';
+export { PubSubAsyncIterator } from './pubsub-async-iterator';
+export { PubSubEngineWithCleanup } from './amqp/interfaces';
